0ca955edd247827e05f1f337b4abba646f3fee22
Erik Hildinge <erik.hildinge@gmail.com>
2020-09-06 01:01:17 +0200

    complete the second assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+function MyInfo() {
+  const info = (
+    <div>
+      <h1>Erik Hildinge</h1>
+      <p>
+        I like horses and to play basketball.<br></br>The three vacations spots
+        i would like to visit are:
+      </p>
+      <ol>
+        <li>Malmö</li>
+        <li>Gran Canaria</li>
+        <li>The Vatican</li>
+      </ol>
+    </div>
+  );
+  return info;
+}
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
