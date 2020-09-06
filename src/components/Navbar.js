8967f94419d08662433858afaf44d4276f627566
Erik Hildinge <erik.hildinge@gmail.com>
2020-09-06 14:54:04 +0200

    complete the parent/child component practise

---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+  return <nav>This is the navigation bar</nav>;
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-function MyInfo() {
-  const info = (
-    <div>
-      <h1>Erik Hildinge</h1>
-      <p>
-        I like horses and to play basketball.<br></br>The three vacations spots
-        i would like to visit are:
-      </p>
-      <ol>
-        <li>Malmö</li>
-        <li>Gran Canaria</li>
-        <li>The Vatican</li>
-      </ol>
-    </div>
-  );
-  return info;
-}
-
-ReactDOM.render(<MyInfo />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
