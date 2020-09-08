ff826dc58481ea6ab1f72c75f084a9c88fbd1b8d
Erik Hildinge <erik.hildinge@gmail.com>
2020-09-08 23:37:26 +0200

    complete the tutorial regarding props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React from "react";
-import Checklist from "./components/Checklist";
+import ContactCard from "./components/ContactCard";
 
 function App() {
-  return <Checklist />;
+  return (
+    <div className="contacts">
+      <ContactCard
+        imageUrl="http://placekitten.com/300/200"
+        name="Mr. Whiskerson"
+        phone="(+46) 788104813"
+        email="mail1@gmail.com"
+      />
+
+      <ContactCard
+        imageUrl="http://placekitten.com/400/200"
+        name="Fluffy"
+        phone="(+46) 818417402"
+        email="mail2@gmail.com"
+      />
+
+      <ContactCard
+        imageUrl="http://placekitten.com/400/300"
+        name="Felix"
+        phone="(+46) 847104185"
+        email="mail3@gmail.com"
+      />
+
+      <ContactCard
+        imageUrl="http://placekitten.com/200/100"
+        name="Rex"
+        phone="(+46) 573071954"
+        email="mail4@gmail.com"
+      />
+    </div>
+  );
 }
 
 export default App;
