667a8e76a6f5a3f9f6977040d8ca3268ad1f4892
Erik Hildinge <erik.hildinge@gmail.com>
2020-09-09 16:51:20 +0200

    map the joke components instead of writing them in Joke.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
 import Joke from "./components/Joke";
+import jokesData from "./jokesData";
 
 function App() {
+  const jokeComponent = jokesData.map((joke) => {
+    return <Joke key={joke.id} question={joke.question} answer={joke.answer} />;
+  });
+
   return (
     <div>
       <header className="header">Jokes</header>
-      <Joke
-        question="What's the best thing about Switzerland?"
-        answer="I don't know, but the flag is a big plus."
-      />
-      <Joke question="I invented a new word!" answer="Plagiarism!" />
-      <Joke answer="Did you hear about the semi-colon that broke the law? He was given two consecutive sentences." />
-      <Joke answer="I got my daughter a fridge for her birthday. I can't wait to see her face light up when she opens it." />
-      <Joke
-        question="What do you get from a pampered cow?"
-        answer="Spoiled milk"
-      />
+      {jokeComponent}
     </div>
   );
 }
