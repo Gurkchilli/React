12117c2bc278b751eb2426815db1292c7aef34ce
Erik Hildinge <erik.hildinge@gmail.com>
2020-09-29 22:49:19 +0200

    change style of the completed items, completing the exercise

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,19 @@
 import React from "react";
 
+//Grå, italics, och linje igenom
+
 function TodoItem(props) {
   return (
     <div className="todo-item">
       <input
         type="checkbox"
-        name="checkbox"
+        className="checkbox"
         checked={props.prop.completed}
         onChange={() => props.handleChange(props.prop.id)}
-        className="checkbox"
       ></input>
-      <label htmlFor="checkbox">{props.prop.text}</label>
+      <label className={props.prop.completed && "completed"} htmlFor="checkbox">
+        {props.prop.text}
+      </label>
     </div>
   );
 }
