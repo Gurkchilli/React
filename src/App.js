ac50a6620437945ffb52900294abae866b7f34ed
Erik Hildinge <erik.hildinge@gmail.com>
2020-09-30 01:23:09 +0200

    fetch data regarding Luke Skywalker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: "",
+      datafetched: false,
+    };
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://swapi.dev/api/people/1")
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState(() => {
+          console.log(data);
+          return {
+            txt: JSON.stringify(data, null, 2),
+            datafetched: true,
+          };
+        })
+      );
+  }
+
+  render() {
+    const displayText = this.state.datafetched ? this.state.txt : "Loading";
+    return (
+      <div>
+        <br />
+        <span>{displayText}</span>
+      </div>
+    );
+  }
 }
 
 export default App;
