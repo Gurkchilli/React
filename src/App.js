bdcd3b70cd6e57f273c736d6d143c9a083276321
Erik Hildinge <erik.hildinge@gmail.com>
2020-10-05 17:21:54 +0200

    add a lot of examples with forms

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ class App extends Component {
     this.state = {
       firstName: "",
       lastName: "",
+      isFriendly: true,
+      gender: "",
+      favColor: "blue",
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
+    const { name, value, type, checked } = event.target;
+    if (type === "checkbox") {
+      this.setState({ [name]: checked });
+    } else {
+      this.setState({ [name]: value });
+    }
   }
 
   render() {
@@ -38,6 +46,51 @@ class App extends Component {
         <h1>
           {this.state.firstName} <br /> {this.state.lastName}
         </h1>
+
+        <br />
+
+        <input
+          type="checkbox"
+          name="isFriendly"
+          checked={this.state.isFriendly}
+          onChange={this.handleChange}
+        />
+        {" Is friendly?"}
+
+        <br />
+
+        <input
+          type="radio"
+          name="gender"
+          value="male"
+          checked={this.state.gender === "male"}
+          onChange={this.handleChange}
+        />
+        {" Male"}
+        <br />
+        <input
+          type="radio"
+          name="gender"
+          value="female"
+          checked={this.state.gender === "female"}
+          onChange={this.handleChange}
+        />
+        {" Female"}
+        <h3>You are a {this.state.gender}</h3>
+
+        <select
+          value={this.state.favColor}
+          onChange={this.handleChange}
+          name="favColor"
+        >
+          <option value="blue">Blue</option>
+          <option value="green">Green</option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+        </select>
+        <h3>Your favorite color is {this.state.favColor}</h3>
+        <button>Submit</button>
       </form>
     );
   }
